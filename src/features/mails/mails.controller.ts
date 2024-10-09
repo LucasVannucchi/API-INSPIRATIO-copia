cdace757b1c555d20a003652c6ef36ed52b5db52
LucasVannucchi <lucasvannucchi16@gmail.com>
2024-10-08 22:41:12 -0300

    Att codigo limpo

--- src/features/mails/mails.controller.ts
+++ src/features/mails/mails.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { MailsService } from "./mails.service";
-import { MailsDTO } from "./dtos/create-mail.dto";
+import { MailsService } from './mails.service';
+import { MailsDTO } from './dtos/create-mail.dto';
 
 @ApiTags('Mails')
 @Controller('mails')
--- src/features/mails/mails.service.ts
+++ src/features/mails/mails.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()

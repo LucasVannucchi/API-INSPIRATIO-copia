cb986867ae59ea31e01690b0b078ad50cde75cf6
Juan Guillherme <codeinnove@gmail.com>
2024-10-11 16:45:35 -0300

    att

--- src/entities/speciality.entity.ts
+++ src/entities/speciality.entity.ts
@@ -8,7 +8,6 @@ export const specialitySchema = new Schema({
   _id: { type: Schema.Types.ObjectId, required: true, auto: true },
   name: { type: String, required: true },
   desciption: {type: String, required: true},
-  password: { type: String, required: false, select: false }
 });
 
 export interface IspecialityEntity extends Omit<Speciality, '_id'>, Document {}

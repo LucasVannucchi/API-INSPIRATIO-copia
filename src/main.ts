87d8ccce99ba1008c582c7d33959d2a9475e28e6
Juan Guillherme <codeinnove@gmail.com>
2024-10-16 13:36:47 -0300

    comment validation pip

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import { urlencoded } from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  //validação dos dados de forma automatica
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();

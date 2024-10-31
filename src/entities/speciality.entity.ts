e228ca548a10125ef3a0a3438fc9ce2f44bd11d6
Juan Guillherme <codeinnove@gmail.com>
2024-10-31 12:42:56 -0300

    att speciality

--- src/entities/speciality.entity.ts
+++ src/entities/speciality.entity.ts
@@ -3,6 +3,7 @@ import { Roles } from '../types/Roles';
 import { Speciality } from 'src/types/speciality';
 
 
+
 // atendimentos
 export const specialitySchema = new Schema({
   _id: { type: Schema.Types.ObjectId, required: true, auto: true },

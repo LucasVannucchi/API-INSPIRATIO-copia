4fe08b7575adf1abc08e2435e1cf108788138651
Juan Guillherme <codeinnove@gmail.com>
2024-10-31 20:33:57 -0300

    modules auth jwt

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtModule } from '@nestjs/jwt';
     UsersModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '12h' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   providers: [AuthService, UserValidator, JwtStrategy],

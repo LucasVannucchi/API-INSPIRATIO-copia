4a3edd1dfff5900a7c2fdf6f3dcb69013de09f4e
Juan Guillherme <codeinnove@gmail.com>
2024-11-11 13:30:55 -0300

    excluindo Query email noo dto

--- src/features/speciality/dto/query_speciality.dto.ts
+++ src/features/speciality/dto/query_speciality.dto.ts
@@ -23,11 +23,6 @@ export class QuerySpecialityDto {
   @ApiProperty({ required: false })
   role?: string;
 
-  @IsOptional()
-  @IsEmail()
-  @ApiProperty({ required: false })
-  email?: string;
-
   @IsOptional() 
   @IsDate() //Valida que o valor fornecido para o campo é uma data válida (Date).
   @ApiProperty({ required: false }) //Este decorator é específico do Swagger, utilizado para gerar documentação de APIs.

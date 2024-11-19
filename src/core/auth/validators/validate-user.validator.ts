820d9672ee3713b4954043c82e524269d83cb54a
Juan Guillherme <codeinnove@gmail.com>
2024-11-18 23:06:26 -0300

    add comentary  admin

--- src/core/auth/validators/validate-user.validator.ts
+++ src/core/auth/validators/validate-user.validator.ts
@@ -2,6 +2,7 @@ import { User } from 'src/types/User';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { HmacSHA512 } from 'crypto-js';
 import { UserService } from 'src/features/users/users.service';
+import { Admin } from 'src/types/admin';
 
 @Injectable()
 export class UserValidator {
@@ -31,4 +32,31 @@ export class UserValidator {
 
     return user;
   }
+
+  /*
+  async validateAdmin(email: string, password: string): Promise<Admin> {
+    const admin: User | undefined = await this.usersService.findByEmail(email);
+
+    if (admin) {
+      throw new HttpException(
+        { message: 'User not found' },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const hashPassword: string = HmacSHA512(
+      password,
+      process.env.PASSWORD_SALT,
+    ).toString();
+
+    if (hashPassword !== admin.password) {
+      throw new HttpException(
+        { message: 'Invalid password' },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    return admin;
+  }*/
+
 }
